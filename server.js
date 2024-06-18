487aabb993ac27d15ba3cc81ecea7d2483e61bb1
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-18 12:05:39 +0800

    added new lines of code and am now hosted on render

--- server.js
+++ server.js
@@ -30,8 +30,15 @@ app.get('/api/:teaName', (request,response) => {
     
 })
 
-app.listen((PORT), () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log("Your teas are here")
 })
+
+
+//can use a git ignore file instead following the instructions below.
+
+//1) touch gitignore
+//2) type node_modules in this file just as it is typed here.
+//3) //watch leons class 38 vide starting at about 3:00
 
 
